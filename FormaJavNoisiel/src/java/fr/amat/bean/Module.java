4bf400379d22d6fefb65de56cde5cbd35768c513
testw10 <testw10@DESKTOP-I7KE0UE>
2017-04-10 16:38:24 +0200

    création bean Module

---
+++ FormaJavNoisiel/src/java/fr/amat/bean/Module.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.amat.bean;
+
+/**
+ *
+ * @author testw10
+ */
+public class Module {
+    
+  private String intitule;
+	private String description;
+	private int nbJour;
+
+	public Module() {
+	}
+
+	public String getIntitule() {
+		return intitule;
+	}
+
+	public void setIntitule(String intitule) {
+		this.intitule = intitule;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getNbJour() {
+		return nbJour;
+	}
+
+	public void setNbJour(int nbJour) {
+		this.nbJour = nbJour;
+	}
+
+    @Override
+    public String toString() {
+        return "Module{" + "intitule=" + intitule + ", description=" + description + ", nbJour=" + nbJour + '}';
+    }
+        
+        
+    
+}
