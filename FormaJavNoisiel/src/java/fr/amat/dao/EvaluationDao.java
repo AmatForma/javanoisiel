7cc3b68a61b176d9683e445ee81ec20cce8e46ca
scamo <scamo@LAPTOP-3TBB1PF9>
2017-04-13 11:52:12 +0200

    ajout des fonctionalites d'evaluation (classe + evaluationdao.java

--- FormaJavNoisiel/src/java/fr/amat/dao/EvaluationDao.java
+++ FormaJavNoisiel/src/java/fr/amat/dao/EvaluationDao.java
@@ -5,6 +5,8 @@
  */
 package fr.amat.dao;
 
+import fr.amat.bean.Evaluation;
+import fr.amat.bean.Personne;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -13,5 +15,97 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-import fr.amat.bean.Personne;
 
+public class EvaluationDao {
+    public static List<Evaluation> fgetALL() throws SQLException{
+		
+		List<Evaluation> result = new ArrayList<Evaluation>();
+		Connection connexion = AmatDao.getConnection();
+		String sql = "SELECT * FROM evaluation";
+		Statement ordre = connexion.createStatement();
+		ResultSet re1 = ordre.executeQuery(sql);
+		while (re1.next()) {
+               Evaluation evaluation = new Evaluation(re1.getString("dateEffet")) ;
+
+                                                         
+                                
+                               
+                        
+			result.add(evaluation);
+		}
+		
+		
+		return result;
+	}
+    
+    
+
+public class PersonneDao {
+	
+	public static Personne getByLoginPassword(String login, String password) throws SQLException{
+		Personne res = null;
+		
+		String sql = "SELECT * FROM personne WHERE mail=?AND password=?";
+		
+		Connection connection = AmatDao.getConnection();
+		PreparedStatement  ordre = connection.prepareStatement(sql);
+		
+		ordre.setString(1, login);
+		ordre.setString(2, password);
+		
+		ResultSet resultat = ordre.executeQuery();
+		
+		if(resultat.next()){
+			res = new Personne(login, password);
+                        res.setIdPersonne(resultat.getInt("idPersonne"));
+                        res.setNom(resultat.getString("nom"));
+                        res.setPrenom(resultat.getString("prenom"));
+					}
+		
+		
+		return res;
+	}
+	
+	
+    
+   /* public static Formation insert(Formation formation) throws SQLException{
+		
+		
+		String sql = "INSERT INTO formation (intitule,description) VALUES (?, ?)";
+		Connection connexion = AmatDao.getConnection();
+		PreparedStatement ordre = connexion.prepareStatement(sql);
+		ordre.setString(1, formation.getIntitule());
+		ordre.setString(2, formation.getDescription());
+		
+		
+		ordre.execute();
+		
+		
+		return null;
+	}
+    
+    
+    
+        
+    public static Formation update(Formation formation) throws SQLException{
+		
+		
+		String sql = "UPDATE  formation SET intitule = ? , description = ?";
+		Connection connexion = AmatDao.getConnection();
+		PreparedStatement ordre = connexion.prepareStatement(sql);
+		ordre.setString(1, formation.getIntitule());
+		ordre.setString(2, formation.getDescription());
+		
+		
+		ordre.execute();
+		
+		
+		return null;
+	}
+    
+    
+    */
+    
+    
+
+}
